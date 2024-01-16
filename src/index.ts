c5107df4dca18714c518cfad7f79da5af894981e
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-01-16 12:29:29 +0500

    Apollo/client Server setup with express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4"
 
-const app = express()
-const PORT = Number(process.env.PORT) || 8000;
-app.get('/', (req, res) => {
-    res.json({ message: "Server is running for the graphql" })
-})
+async function init() {
+    const app = express()
+    const PORT = Number(process.env.PORT) || 8000;
+    app.use(express.json())
 
-app.listen(PORT, () => {
-    console.log(`Server Started at http://localhost:${PORT}`)
-});
+    // Create a gqlServer
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+            type Query{
+                hello: String
+                say(name: String!): String
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => `Hey there, I am a graphql server`,
+                say: (_,{name})=> `Hey ${name}, How you doinnggg....`
+            },
+        },
+    });
+
+    // Start the gqlServer
+    await gqlServer.start();
+
+    app.get('/', (req, res) => {
+        res.json({ message: "Server is running for the graphql" })
+    })
+
+    app.use("/graphql", expressMiddleware(gqlServer));
+
+    app.listen(PORT, () => {
+        console.log(`Server Started at http://localhost:${PORT}`)
+    });
+}
+
+init();
