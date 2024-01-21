5616658f08dd394e593ff8e436897a03be6be9e7
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-01-21 22:04:59 +0500

    make lastName optional in db, make user Service of create user

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,10 +1,13 @@
+import UserService, { CreateUserPayload } from "../../services/user";
+
 const queries = {
     hello: () => `Hy, How are you`
 };
 const mutations = {
-    createUser: async (_: any, { }: {}) => {
-        return "randomid"
-    }
+    createUser: async (_: any, payload: CreateUserPayload) => {
+        const res = await UserService.createUser(payload);
+        return res.id;
+    },
 };
 
 export const resolvers = { queries, mutations };
